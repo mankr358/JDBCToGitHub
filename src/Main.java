846f36f367c952b7335c41a96efbee5fd22c138f
Manish Kumar <manishkumaryadav7472@gmail.com>
2024-09-06 19:06:12 +0530

    add and update data inside it and done yourself delete opertations

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,8 @@ public class Main {
             Connection connection = DriverManager.getConnection(url,username,password);
             //Statement statement = connection.createStatement();
            // String query = "INSERT INTO students(name, age, marks) VALUES(?,?,?)";
-            String query =  "SELECT marks FROM students WHERE id = ?";
+           // String query =  "SELECT marks FROM students WHERE id = ?";
+            String query = "UPDATE students SET marks = ? WHERE ID = ?";
 
              // String query = String.format("INSERT INTO students(name, age, marks) VALUES('%s', %o, %f)","Rhaul", 23, 74.5);
            // String query = String.format("UPDATE students SET marks = %f WHERE id = %d", 89.7,2);
@@ -27,26 +28,28 @@ public class Main {
 //            preparedStatement.setString(1, "Ankita");
 //            preparedStatement.setInt(2,25);
 //            preparedStatement.setDouble(3,84.7);
-            preparedStatement.setInt(1,1);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()){
-                double marks = resultSet.getDouble("marks");
-                System.out.println("Marsks: " + marks);
-            }else{
-                System.out.println("Marks Not found: ");
-            }
+//            preparedStatement.setInt(1,1);
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            if(resultSet.next()){
+//                double marks = resultSet.getDouble("marks");
+//                System.out.println("Marsks: " + marks);
+//            }else{
+//                System.out.println("Marks Not found: ");
+//            }
+            preparedStatement.setDouble(1,87.5);
+            preparedStatement.setInt(2,3);
 
             // String query = "select * from students";
             //this is used for retrival data but if check for update; and hold the data by ResultSet
 //            ResultSet resultSet = statement.executeQuery(query);
            // int rowsAffected  = statement.executeUpdate(query);
 
-//            int rowsAffected = preparedStatement.executeUpdate();
-//            if(rowsAffected > 0){
-//                System.out.println("Data Inserted Successfully");
-//            }else{
-//                System.out.println(" Data Not Inserted");
-//            }
+            int rowsAffected = preparedStatement.executeUpdate();
+            if(rowsAffected > 0){
+                System.out.println("Data Inserted Successfully");
+            }else{
+                System.out.println(" Data Not Inserted");
+            }
 
 //            while(resultSet.next()){
 //                int id = resultSet.getInt("id");
