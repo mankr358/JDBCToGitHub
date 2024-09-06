eaafe4df664d77b505a419b7ce21e6559f32de42
Manish Kumar <manishkumaryadav7472@gmail.com>
2024-09-06 17:01:51 +0530

    first commit and showing data in my intellij that fetch to mysql

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+
+import static java.lang.Class.forName;
+
+public class Main {
+    private static final String url = "jdbc:mysql://localhost:3306/mydb";
+
+    private static final String username = "root";
+    private static final String password = "M1n8shkum1r@#";
+
+    public static void main(String[] args) {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Statement statement = connection.createStatement();
+            String query = "select * from students";
+            //this is used for retrival data but if check for update; and hold the data by ResultSet
+            ResultSet resultSet = statement.executeQuery(query);
+            while(resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                double marks = resultSet.getDouble("marks");
+                System.out.println("ID: " +id );
+                System.out.println("NAME: " + name);
+                System.out.println("AGE: " +age);
+                System.out.println("MARKS" + marks);
+            }
+
+
+        }catch(SQLException e){
+
+        }
+    }
+}
