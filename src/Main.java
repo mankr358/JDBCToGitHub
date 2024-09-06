3ddc326bd900ab5a1a866af199f2a8f397bae192
Manish Kumar <manishkumaryadav7472@gmail.com>
2024-09-06 18:56:41 +0530

    Update in single entity related changed

--- src/Main.java
+++ src/Main.java
@@ -17,26 +17,37 @@ public class Main {
         try{
             Connection connection = DriverManager.getConnection(url,username,password);
             //Statement statement = connection.createStatement();
-            String query = "INSERT INTO students(name, age, marks) VALUES(?,?,?)";
+           // String query = "INSERT INTO students(name, age, marks) VALUES(?,?,?)";
+            String query =  "SELECT marks FROM students WHERE id = ?";
 
              // String query = String.format("INSERT INTO students(name, age, marks) VALUES('%s', %o, %f)","Rhaul", 23, 74.5);
            // String query = String.format("UPDATE students SET marks = %f WHERE id = %d", 89.7,2);
           //  String query = "DELETE FROM students WHERE ID = 2";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, "Ankita");
-            preparedStatement.setInt(2,25);
-            preparedStatement.setDouble(3,84.7);
+//            preparedStatement.setString(1, "Ankita");
+//            preparedStatement.setInt(2,25);
+//            preparedStatement.setDouble(3,84.7);
+            preparedStatement.setInt(1,1);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                double marks = resultSet.getDouble("marks");
+                System.out.println("Marsks: " + marks);
+            }else{
+                System.out.println("Marks Not found: ");
+            }
 
             // String query = "select * from students";
             //this is used for retrival data but if check for update; and hold the data by ResultSet
 //            ResultSet resultSet = statement.executeQuery(query);
            // int rowsAffected  = statement.executeUpdate(query);
-            int rowsAffected = preparedStatement.executeUpdate();
-            if(rowsAffected > 0){
-                System.out.println("Data Inserted Successfully");
-            }else{
-                System.out.println(" Data Not Inserted");
-            }
+
+//            int rowsAffected = preparedStatement.executeUpdate();
+//            if(rowsAffected > 0){
+//                System.out.println("Data Inserted Successfully");
+//            }else{
+//                System.out.println(" Data Not Inserted");
+//            }
+
 //            while(resultSet.next()){
 //                int id = resultSet.getInt("id");
 //                String name = resultSet.getString("name");
